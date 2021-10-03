9650006b659ed0618088872d37abdc355b54d846
Andrea Benedetto <34688096+benedettoandrea@users.noreply.github.com>
2021-10-03 20:17:47 +0200

    initial project structure
    - added responsive code from assignment #1
    - added theme switcher
    - added temporary external JSON
    - added temporary p5 sketch

--- sketch.js
+++ sketch.js
@@ -1,13 +1,29 @@
+var backgroundToggle = 0;
+var palette;
+
 function preload() {
-  // put preload code here
+  palette = loadJSON("100.json");
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  // put setup code here
+  createCanvas(windowWidth, windowHeight, WEBGL).parent("container");
+  frameRate(60);
+
+  //noStroke();
+  console.log(palette[0][1]);
+  fill(palette[0][1]);
 }
 
 function draw() {
-  // put drawing code here
-  background("red");
+  background(0, 0, 0, 0);
+  rotateX(frameCount * 0.1);
+  plane(20, 20);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function saveScreenshot() {
+  saveCanvas("myCanvas", "png");
 }
---
+++ theme.js
@@ -0,0 +1,14 @@
+// function to set a given theme
+function setTheme(themeName) {
+  localStorage.setItem("theme", themeName);
+  document.documentElement.className = themeName;
+}
+
+// function to toggle between light and dark theme
+function toggleTheme() {
+  if (localStorage.getItem("theme") === "dark") {
+    setTheme("light");
+  } else {
+    setTheme("dark");
+  }
+}
