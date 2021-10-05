475b830365512335701cbf3ee3452d3c9f745b97
Andrea Benedetto <34688096+benedettoandrea@users.noreply.github.com>
2021-10-06 00:50:06 +0200

    finalisation of the concept
    - temporary colour gradient
    - it still needs some polish (css, features, etc.)

--- sketch.js
+++ sketch.js
@@ -1,7 +1,10 @@
 var backgroundToggle = 255;
-var palette;
 var xDim = 20;
 var yDim = 20;
+var angleX = 0;
+var angleY = 0;
+var octaves = 8;
+var falloff = 0.5;
 
 function preload() {
   palette = loadJSON("100.json");
@@ -12,11 +15,12 @@ function setup() {
   frameRate(60);
   fill(255);
   stroke(0);
-  strokeWeight(windowHeight / 500);
+  strokeWeight(windowHeight / 750);
   angleMode(DEGREES);
 }
 
 function draw() {
+  noiseDetail(octaves, falloff);
   background(backgroundToggle);
   ortho(-width / 2, width / 2, -height / 2, height / 2, -100000, 100000);
 
@@ -30,14 +34,44 @@ function draw() {
       y < height / 2;
       y += height / yDim
     ) {
+      let noiseColor = noise(
+        (frameCount / 5 + x) / 250,
+        (frameCount / 5 + y) / 250
+      );
       push();
       translate(x, y);
-      rotateX(frameCount * 0.1);
-      rotateY(frameCount * 0.1);
+      rotateX(angleX);
+      rotateY(angleY);
+      if (Math.floor(noiseColor * 10) == 0) {
+        fill(230, 81, 0);
+      } else if (Math.floor(noiseColor * 10) == 1) {
+        fill(239, 108, 0);
+      } else if (Math.floor(noiseColor * 10) == 2) {
+        fill(245, 124, 0);
+      } else if (Math.floor(noiseColor * 10) == 3) {
+        fill(251, 140, 0);
+      } else if (Math.floor(noiseColor * 10) == 4) {
+        fill(255, 152, 0);
+      } else if (Math.floor(noiseColor * 10) == 5) {
+        fill(255, 167, 38);
+      } else if (Math.floor(noiseColor * 10) == 6) {
+        fill(255, 183, 77);
+      } else if (Math.floor(noiseColor * 10) == 7) {
+        fill(255, 204, 128);
+      } else if (Math.floor(noiseColor * 10) == 8) {
+        fill(255, 224, 178);
+      } else {
+        fill(255, 243, 224);
+      }
       plane(windowWidth / xDim, windowHeight / yDim);
       pop();
     }
   }
+}
+
+function mouseDragged() {
+  angleX = map(mouseY, -width / 2, width / 2, 360, 0);
+  angleY = map(mouseX, -width / 2, width / 2, 360, 0);
 }
 
 // generate a random integer from range, inclusive
@@ -59,11 +93,9 @@ function switchBackground() {
   if (backgroundToggle === 255) {
     backgroundToggle = 0;
     stroke(255);
-    fill(0);
   } else if (backgroundToggle === 0) {
     backgroundToggle = 255;
     stroke(0);
-    fill(255);
   }
 }
 
