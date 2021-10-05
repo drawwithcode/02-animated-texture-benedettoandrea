c684237cc89dcee693459aaecdec88dccf75eecc
Andrea Benedetto <34688096+benedettoandrea@users.noreply.github.com>
2021-10-05 03:01:09 +0200

    modified entity spawning
    - planes instead of rectangles
    - possibly, more flexible code
    - temporary animation and colours

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 var backgroundToggle = 255;
 var palette;
-var xDim = 25;
-var yDim = 25;
+var xDim = 20;
+var yDim = 20;
 
 function preload() {
   palette = loadJSON("100.json");
@@ -10,17 +10,32 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL).parent("container");
   frameRate(60);
-  fill(palette[0][1]);
+  fill(255);
   stroke(0);
-  strokeWeight(2.5);
+  strokeWeight(windowHeight / 500);
+  angleMode(DEGREES);
 }
 
 function draw() {
-  ortho(-width / 2, width / 2, -height / 2, height / 2, -100000, 100000);
   background(backgroundToggle);
-  for (var x = -width / 2; x < width / 2; x += width / xDim) {
-    for (var y = -height / 2; y < height / 2; y += height / yDim) {
-      rect(x, y, windowWidth / xDim, windowHeight / yDim);
+  ortho(-width / 2, width / 2, -height / 2, height / 2, -100000, 100000);
+
+  for (
+    var x = -width / 2 + width / xDim / 2;
+    x < width / 2;
+    x += width / xDim
+  ) {
+    for (
+      var y = -height / 2 + height / yDim / 2;
+      y < height / 2;
+      y += height / yDim
+    ) {
+      push();
+      translate(x, y);
+      rotateX(frameCount * 0.1);
+      rotateY(frameCount * 0.1);
+      plane(windowWidth / xDim, windowHeight / yDim);
+      pop();
     }
   }
 }
@@ -44,8 +59,36 @@ function switchBackground() {
   if (backgroundToggle === 255) {
     backgroundToggle = 0;
     stroke(255);
+    fill(0);
   } else if (backgroundToggle === 0) {
     backgroundToggle = 255;
     stroke(0);
+    fill(255);
+  }
+}
+
+function AddWidth() {
+  xDim++;
+}
+
+function RemWidth() {
+  xDim--;
+}
+
+function AddHeight() {
+  yDim++;
+}
+
+function RemHeight() {
+  yDim--;
+}
+
+function keyReleased() {
+  if (key == "s" || key == "S") {
+    if (isLooping()) {
+      noLoop();
+    } else {
+      loop();
+    }
   }
 }
